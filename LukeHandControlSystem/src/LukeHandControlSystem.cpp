7e38e5731e7cfe977a007386a9ab3c397752f1b9
dnearhos <david@nearhos.com>
2016-09-25 11:09:38 +1000

    Starting point for the LukeHand Control System.

---
+++ LukeHandControlSystem/src/LukeHandControlSystem.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : LukeHandControlSystem.cpp
+// Author      : David
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "This is the a starting point for the LukeHand Control System" << endl;
+	return 0;
+}
